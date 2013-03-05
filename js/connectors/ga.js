fe2bce339147691d75d33c00ef5bd1c0977bfd48
Cedric Dugas <cedric.dugas@gmail.com>
2013-03-05 13:00:49 -0500

    fix ie7

--- js/connectors/ga.js
+++ js/connectors/ga.js
@@ -2,11 +2,12 @@
 	$.behaviorMiner.connectors.ga = {
 		init : function()  {
 			$(document).on("log_user_behavior", function(e,data){
+				var content = (window.JSON) ? JSON.stringify(data) : "";
 				_gaq.push([
 					'_trackEvent',
 					data.type,
 					data.url,
-					JSON.stringify(data)
+					content
 				]);
 			});
 		}
